6366bf6f4d5b47e69d2b8502d9357615b735d067
Mohit <you@example.com>
2018-10-22 11:47:54 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
+import { FingerprintAIO} from '@ionic-native/fingerprint-aio';
+import { SecureStorage } from '@ionic-native/secure-storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -22,8 +25,11 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    Camera,
     StatusBar,
     SplashScreen,
+    FingerprintAIO,
+    SecureStorage, 
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,135 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
+
+
+import { SecureStorage, SecureStorageObject } from '@ionic-native/secure-storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private faio: FingerprintAIO ,private secureStorage: SecureStorage) {
 
   }
 
+ngOnInit(){
+  
+ 
+}
+
+fingerprint(){
+  this.faio.show({
+    clientId: 'Fingerprint-Demo',
+    clientSecret: 'password', //Only necessary for Android
+    disableBackup:true,  //Only for Android(optional)
+    localizedFallbackTitle: 'Use Pin', //Only for iOS
+    localizedReason: 'Please authenticate' //Only for iOS
+})
+.then((result: any) => console.log(result))
+.catch((error: any) => console.log(error));
+}
+
+getCredentialsFromKeyStore(){
+  this.secureStorage.create('ionic_app2_store_name')
+  .then((storage: SecureStorageObject) => {
+
+    //  storage.get('key')
+    //    .then(
+    //      data => console.log(data),
+    //      error => console.log(error)
+    //  );
+
+    //  storage.set('key', 'value')
+    //    .then(
+    //     data => console.log(data),
+
+    //      error => console.log(error)
+    //  );
+
+    //  storage.remove('key')
+    //  .then(
+    //      data => console.log(data),
+    //      error => console.log(error)
+    //  );
+    
+
+     storage.get("testKey").then(data => alert(data),error =>console.log(error));
+
+  });
+}
+
+setCredentialsInKeyStore(){
+  this.secureStorage.create('ionic_app2_store_name')
+  .then((storage: SecureStorageObject) => {
+
+    //  storage.get('key')
+    //    .then(
+    //      data => console.log(data),
+    //      error => console.log(error)
+    //  );
+
+    //  storage.set('key', 'value')
+    //    .then(
+    //     data => console.log(data),
+
+    //      error => console.log(error)
+    //  );
+
+    //  storage.remove('key')
+    //  .then(
+    //      data => console.log(data),
+    //      error => console.log(error)
+    //  );
+     storage.set("testKey","testValue4").then(
+           data => {alert(data),
+           alert("sucessfully set");},
+           error => console.log(error)
+       );
+
+     
+
+  });
+}
+
+  deleteCredentialsFromKeyStore(){
+    this.secureStorage.create('ionic_app2_store_name')
+    .then((storage: SecureStorageObject) => {
+  
+      //  storage.get('key')
+      //    .then(
+      //      data => console.log(data),
+      //      error => console.log(error)
+      //  );
+  
+      //  storage.set('key', 'value')
+      //    .then(
+      //     data => console.log(data),
+          
+      //      error => console.log(error)
+      //  );
+  
+      //  storage.remove('key')
+      //  .then(
+      //      data => console.log(data),
+      //      error => console.log(error)
+      //  );
+       
+      storage.set("testKey",null).then(
+        data => {alert(data),
+        alert("sucessfully set null");},
+        error => console.log(error)
+    );
+      
+       storage.remove("testKey").then(
+         data=> alert('remove' + data),
+         error=>console.log(error)
+    )
+       
+  
+    });
+  
+}
 }
